e5f57fd0b272e42793880bd9dd94bdcece5a2afc
sam hockaday <samuel.hockaday@gmail.com>
2023-02-06 22:02:19 +0000

    Java and html to clock additions made

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
-function realtimewebclock() {
-    var rtClock = new Date();
+function displayTime(){
+    var dateTime = new Date();
+    var hours = dateTime.getHours();
+    var minutes = dateTime.getMinutes;
+    var seconds = dateTime.getSeconds;
+   var session = document.getElementById('session')
+   
+   if(hours >= 12){
+    session.innerHTML = 'PM';
+   }else{
+    session.innerHTML = 'AM';
+   }
+ 
+   document.getElementById('hours').innerHTML = hours;
+   document.getElementById('minutes').innerHTML = minutes;
+   document.getElementById('seconds').innerHTML = seconds;
 
-    var hours = rtClock.getHours();
-    var minutes = rtClock.getMinutes;
-    var seconds = rtClock.getSeconds;
-   var session = document.getElementById
-    //add AM and PM system 
-    var amPm = ( hours < 12) ? "AM" : "PM";
+
+   }
+
+   //add AM and PM system 
+
+    
 
 //Convert hours into 12 hour rather than 24 format
-    hours = (hours > 12) ? hours = 12 : hours;
+ 
 
 
     // Add padding to hours, mins and seconds with leading zeros
-    hours =("0" + hours).slice(-2);
-    minutes =("0" + minutes).slice(-2);
-    seconds =("0" + seconds).slice(-2);
-    miliiseconds
+ 
 
 
 
 
     // DISPLAY THE CLOCK ITSELF via JS
-    document.getElementById('Clock').innerHTML =
-    hours + " : " + minutes + " : " + seconds + " " + amPm;
-    var t = setTimeout(realtimewebclock, 500);
+   
     
 }
