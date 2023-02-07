ce961bb77c10d0f928716e2b37fda590de231043
sam hockaday <samuel.hockaday@gmail.com>
2023-02-07 15:41:00 +0000

    Changed JS code to be more efficient.

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 function displayTime(){
     var dateTime = new Date();
-    var hours = dateTime.getHours();
-    var minutes = dateTime.getMinutes;
-    var seconds = dateTime.getSeconds;
-   var session = document.getElementById('session')
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var sec = dateTime.getSeconds();
+   var session = document.getElementById('session');
    
-   if(hours >= 12){
+   if(hrs >= 12){
     session.innerHTML = 'PM';
    }else{
     session.innerHTML = 'AM';
    }
  
-   document.getElementById('hours').innerHTML = hours;
-   document.getElementById('minutes').innerHTML = minutes;
-   document.getElementById('seconds').innerHTML = seconds;
+   document.getElementById('hours').innerHTML = hrs;
+   document.getElementById('minutes').innerHTML = min;
+   document.getElementById('seconds').innerHTML = sec;
 
 
    }
+   setInterval(displayTime, 10);
+
 
    //add AM and PM system 
 
@@ -35,4 +37,3 @@ function displayTime(){
     // DISPLAY THE CLOCK ITSELF via JS
    
     
-}
