00fd6cb28762ed0b4bb418888f85cac6e07e7cc8
sam hockaday <samuel.hockaday@gmail.com>
2023-02-13 20:58:36 +0000

    I fixed an error relating to JS not working.

---
+++ script.js
@@ -0,0 +1,9 @@
+let output = document.getElementById('output');
+let numberDate = document.getElementById('date');
+
+let d = new Date();
+let day =d.getDay();
+let ndate = d.toLocaleString();
+console.log(day);
+
+
