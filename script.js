e6cee6fb71fb46e863622c64bdcd6c2bebe7c000
sam hockaday <samuel.hockaday@gmail.com>
2023-02-13 23:16:14 +0000

    Used JS to produce live date and day.

--- script.js
+++ script.js
@@ -4,6 +4,30 @@ let numberDate = document.getElementById('date');
 let d = new Date();
 let day =d.getDay();
 let ndate = d.toLocaleString();
-console.log(day);
 
+switch (day){
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;   
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3 :
+        day = "Wednesday";
+        break;
+    case 4 :
+        day = "Thursday";
+    case 5 :
+        day = "Friday";
+        break;
+    case 6 :
+        day = "Saturday";                             
+
+}
+
+output.innerHTML = day;
+date.innerHTML = ndate;
 
